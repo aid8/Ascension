aed9a0d6afe1bfc9ac53826bf9fc78c6d00ec51a
Aid <33380942+aid8@users.noreply.github.com>
2022-07-19 23:36:09 +0800

    Updated App.vue
    Sorry I deleted it accidentally.

---
+++ ascension/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <!--
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/AccountCreation">Crete Account</router-link>
+  </nav>
+  -->
+  <router-view/>
+</template>
+
+<script>
+  import Parse from 'parse';
+  //import './assets/tailwind.css';
+  Parse.serverURL = "http://localhost:1337/api";
+  Parse.initialize("myAppId", "jspass");
+
+  export default{
+    components: {
+      
+    },
+  };
+</script>
